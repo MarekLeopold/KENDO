0bc849f192032abd03929febb7dccfb5886b82b5
MarekLeopold <m_leopold@utb.cz>
2022-03-12 14:10:39 +0100

    commit

---
+++ Pool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Pool
+{
+	private Zapasnik[] zapasnici;
+	private int pocetZapasniku;
+
+
+	public Pool(Zapasnik[] zapasnici, int pocetZapasniku)
+	{
+		this.zapasnici = new Zapasnik[3];
+		this.zapasnici = zapasnici;
+		
+	}
+}
---
+++ Zapasnik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Zapasnik
+{
+	private int ID;
+	private string jmeno;
+	private string prijmeni;
+	private byte vek;
+	private byte uroven; // 10kju –> 1 kju -> 1 dan -> 10 dan // 20 urovni (0-19)
+
+
+	public Zapasnik(string jmeno, string prijmeni, byte vek, byte uroven) // rucni vlozeni zapasnika
+	{
+		this.jmeno = jmeno;
+		this.prijmeni = prijmeni;
+		this.vek = vek;
+		this.uroven = uroven;
+		//save
+	}
+
+	public Zapasnik(int ID) // nacteni z DB
+	{
+		// load
+	}
+}
